b39ef6fdc7f3a998be5246e74c40f5c84d18dac1
Anshu Nair <43226299+AnshuNair@users.noreply.github.com>
2020-03-24 15:22:19 -0400

    MajorBugFix

--- Assets/Scripts/MatchFinder.cs
+++ Assets/Scripts/MatchFinder.cs
@@ -1,6 +1,8 @@
 ﻿using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,18 +12,96 @@ public class MatchFinder : MonoBehaviourPunCallbacks
     public Text playButtonText;
     int seconds = 3;
     const byte START_MATCH_EVENT = 0;
-    
-
+    const string characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    int charAmount = 10;
     [SerializeField]
-    private string roomName = "RandomRoom";
+    private string roomName = "";
+    private List<string> allRoomNames;
+    List<RoomInfo> rooms;
+    bool roomListUpdated = false;
+
+    private void Start()
+    {
+        allRoomNames = new List<string>();
+    }
 
     public void CreateOrJoinRoom()
     {
         if (!PhotonNetwork.IsConnected)
             return;
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 2;
-        PhotonNetwork.JoinOrCreateRoom(roomName, options, TypedLobby.Default);
+
+        //Join the lobby to trigger the OnRoomListUpdate callback
+        PhotonNetwork.JoinLobby();
+
+        //Call a coroutine to find the match
+        StartCoroutine(FindMatch());
+    }
+
+    IEnumerator FindMatch()
+    {
+        //Wait until OnRoomListUpdate is successfully called
+        yield return new WaitUntil(() => roomListUpdated = true);
+        yield return new WaitForSeconds(1);
+
+        //Loop through existing rooms to find an open room
+        if (rooms != null)
+        {
+            foreach (RoomInfo r in rooms)
+            {
+                //Open room found
+                if (r.PlayerCount != r.MaxPlayers)
+                {
+                    //Join Room
+                    PhotonNetwork.JoinRoom(r.Name);
+                    //Exit the coroutine
+                    yield break;
+                }
+
+                //Room exists so store it's name
+                allRoomNames.Add(r.Name);
+            }
+        }
+
+        //We didn't find any open games, so let's create one
+        //Generate a random string to use as room name
+        roomName = "";
+        for (int i = 0; i < charAmount; i++)
+        {
+            roomName += characters[Random.Range(0, characters.Length)];
+        }
+
+        //Check if roomName is unique
+        if (rooms != null)
+        {
+            if (allRoomNames != null)
+            {
+                foreach (string n in allRoomNames)
+                {
+                    //Room name is not unique
+                    if (n.Equals(roomName))
+                    {
+                        //generate a new room name
+                        roomName = "";
+                        for (int i = 0; i < charAmount; i++)
+                        {
+                            roomName += characters[Random.Range(0, characters.Length)];
+                        }
+                    }
+                }
+            }
+        }
+
+        RoomOptions options = new RoomOptions
+        {
+            MaxPlayers = 2
+        };
+
+        //Create the room
+        PhotonNetwork.CreateRoom(roomName, options);
+
+        //Clear the list of room names
+        if (allRoomNames != null)
+            allRoomNames.Clear();
     }
 
     public override void OnCreatedRoom()
@@ -59,6 +139,18 @@ public class MatchFinder : MonoBehaviourPunCallbacks
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.Log("Room creation failed: " + message, this);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        //Update the rooms field with the new roomList
+        rooms = roomList;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //To accomodate for network delays, use a boolean and a WaitUntil to reduce chances of error
+        roomListUpdated = true;
     }
 
     void OnDestroy()
