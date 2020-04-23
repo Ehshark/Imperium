28702d0a17b0c2fdb94d52bda50d109586069d69
Anshu Nair <43226299+AnshuNair@users.noreply.github.com>
2020-04-22 23:38:03 -0400

    AddedFieldsToCardData.cs

--- Assets/Scripts/CmdPattern/CardData.cs
+++ Assets/Scripts/CmdPattern/CardData.cs
@@ -9,7 +9,7 @@ public class CardData : ScriptableObject
     [SerializeField]
     private Color color;
     [SerializeField]
-    private string goldAndManaCost;
+    private int goldAndManaCost;
     [SerializeField]
     private string conditionText;
     [SerializeField]
@@ -40,4 +40,22 @@ public class CardData : ScriptableObject
     private object enemyDamageDealt;
     [SerializeField]
     private string allyClass;
+
+    public Color Color { get => color; set => color = value; }
+    public int GoldAndManaCost { get => goldAndManaCost; set => goldAndManaCost = value; }
+    public string ConditionText { get => conditionText; set => conditionText = value; }
+    public object Condition { get => condition; set => condition = value; }
+    public string EffectText1 { get => effectText1; set => effectText1 = value; }
+    public object Effect1 { get => effect1; set => effect1 = value; }
+    public string EffectText2 { get => effectText2; set => effectText2 = value; }
+    public object Effect2 { get => effect2; set => effect2 = value; }
+    public Image Image { get => image; set => image = value; }
+    public int AttackDamage { get => attackDamage; set => attackDamage = value; }
+    public string Health { get => health; set => health = value; }
+    public string CardClass { get => cardClass; set => cardClass = value; }
+    public bool IsPromoted { get => isPromoted; set => isPromoted = value; }
+    public bool IsTapped { get => isTapped; set => isTapped = value; }
+    public bool IsSilenced { get => isSilenced; set => isSilenced = value; }
+    public object EnemyDamageDealt { get => enemyDamageDealt; set => enemyDamageDealt = value; }
+    public string AllyClass { get => allyClass; set => allyClass = value; }
 }
