c485da54eed44943858e5b8283c073fecdbb6bbd
Anshu Nair <43226299+AnshuNair@users.noreply.github.com>
2020-04-25 01:46:07 -0400

    Added-CreateCardAssets.cs

---
+++ Assets/Scripts/CmdPattern/CreateCardAssets.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateCardAssets : MonoBehaviour
+{
+    const Int32 BufferSize = 128;
+    private readonly string path = "D:\\UnityProjects\\Unity-Imperium\\Imperium\\Assets\\allMinions.csv";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FileStream fileStream = File.OpenRead(path);
+        //StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
+        //string line;
+        //line = streamReader.ReadLine();
+        //while ((line = streamReader.ReadLine()) != null)
+        //{
+        //    string[] lineArr = new string[12];
+        //    lineArr = line.Split(',');
+
+        //    CardData newCard = ScriptableObject.CreateInstance<CardData>();
+        //    newCard.Color = ToColor(lineArr[0]);
+        //    newCard.MinionID = Int32.Parse(lineArr[1]);
+        //    newCard.GoldAndManaCost = Int32.Parse(lineArr[2]);
+        //    newCard.ConditionText = lineArr[3];
+        //    newCard.EffectText1 = lineArr[4];
+        //    newCard.EffectText2 = lineArr[5].ToString();
+        //    newCard.AttackDamage = Int32.Parse(lineArr[6]);
+        //    newCard.Health = Int32.Parse(lineArr[7]);
+        //    newCard.CardClass = lineArr[8];
+        //    newCard.IsPromoted = false;
+        //    newCard.IsTapped = false;
+        //    newCard.IsSilenced = false;
+        //    newCard.AllyClass = lineArr[9];
+
+        //    AssetDatabase.CreateAsset(newCard, "Assets/Cards/" + lineArr[1] + ".asset");
+        //}
+    }
+
+    public Color ToColor(string color)
+    {
+        if (color.Equals("Orange"))
+            return new Color(1.0f, 0.5f, 0.0f);
+        else
+            return (Color)typeof(Color).GetProperty(color.ToLowerInvariant()).GetValue(null, null);
+    }
+}
--- Assets/Scripts/CmdPattern/ScriptableObjects/CardData.cs
+++ Assets/Scripts/CmdPattern/ScriptableObjects/CardData.cs
@@ -9,6 +9,8 @@ public class CardData : ScriptableObject
     [SerializeField]
     private Color color;
     [SerializeField]
+    private int minionID;
+    [SerializeField]
     private int goldAndManaCost;
     [SerializeField]
     private Image goldOrManaIcon;
@@ -31,7 +33,7 @@ public class CardData : ScriptableObject
     [SerializeField]
     private int attackDamage;
     [SerializeField]
-    private string health;
+    private int health;
     [SerializeField]
     private Image healthIcon;
     [SerializeField]
@@ -59,6 +61,7 @@ public class CardData : ScriptableObject
 
     public Color Color { get => color; set => color = value; }
     public int GoldAndManaCost { get => goldAndManaCost; set => goldAndManaCost = value; }
+    public int MinionID { get => minionID; set => minionID = value; }
     public string ConditionText { get => conditionText; set => conditionText = value; }
     public object Condition { get => condition; set => condition = value; }
     public string EffectText1 { get => effectText1; set => effectText1 = value; }
@@ -66,11 +69,20 @@ public class CardData : ScriptableObject
     public string EffectText2 { get => effectText2; set => effectText2 = value; }
     public object Effect2 { get => effect2; set => effect2 = value; }
     public int AttackDamage { get => attackDamage; set => attackDamage = value; }
-    public string Health { get => health; set => health = value; }
+    public int Health { get => health; set => health = value; }
     public string CardClass { get => cardClass; set => cardClass = value; }
     public bool IsPromoted { get => isPromoted; set => isPromoted = value; }
     public bool IsTapped { get => isTapped; set => isTapped = value; }
     public bool IsSilenced { get => isSilenced; set => isSilenced = value; }
     public object EnemyDamageDealt { get => enemyDamageDealt; set => enemyDamageDealt = value; }
     public string AllyClass { get => allyClass; set => allyClass = value; }
+    public Image GoldOrManaIcon { get => goldOrManaIcon; set => goldOrManaIcon = value; }
+    public Image ConditionIcon { get => conditionIcon; set => conditionIcon = value; }
+    public Image CardArt { get => cardArt; set => cardArt = value; }
+    public Image HealthIcon { get => healthIcon; set => healthIcon = value; }
+    public Image ClassIcon { get => classIcon; set => classIcon = value; }
+    public Image PromotedIcon { get => promotedIcon; set => promotedIcon = value; }
+    public Image TappedIcon { get => tappedIcon; set => tappedIcon = value; }
+    public Image SilencedIcon { get => silencedIcon; set => silencedIcon = value; }
+    public Image AllyClassIcon { get => allyClassIcon; set => allyClassIcon = value; }
 }
