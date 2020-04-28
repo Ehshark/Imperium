1d9ab37f5d6dbbb499018f8d08c3c3f4baf765e1
Unknown <fxcence@gmail.com>
2020-04-28 18:51:13 -0400

    skill tree overlay rough

---
+++ Assets/SkillTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SkillTree : MonoBehaviour
+{
+
+    public GameObject skillTreePanel;
+    public Text tex;
+    public void Trash()
+    {
+        if (skillTreePanel != null)
+        {
+            tex = skillTreePanel.GetComponentInChildren<Text>();
+            Debug.Log(tex);
+            tex.text = "PLZ YOLO";
+            skillTreePanel.SetActive(true);
+
+        }
+
+    }
+
+    public void confirm ()
+    {
+        SceneManager.LoadScene("GameBoard");
+        Debug.Log("yolo confirm");
+    }
+
+    public void cancel()
+    {
+        SceneManager.LoadScene("GameBoard");
+        Debug.Log("yolo cancel");
+
+    }
+}
