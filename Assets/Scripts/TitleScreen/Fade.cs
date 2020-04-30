8fe83e852c9a392d0210a999291638b41e7eb61c
C067 <callumadodge@gmail.com>
2020-04-29 23:04:21 -0400

    Added Titlescreen

---
+++ Assets/Scripts/TitleScreen/Fade.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    public bool doFade = true;
+
+    public void Update()
+    {
+        if (doFade)
+        {
+            FadeEffect();
+        }
+    }
+
+    public void FadeEffect()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (canvasGroup.alpha == 1)
+        {
+            doFade = false;
+            StartCoroutine(DoFadeOut());
+        }
+        else
+        {
+            doFade = false;
+            StartCoroutine(DoFadeIn());
+        }
+    }
+
+    IEnumerator DoFadeOut ()
+    {
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 1.25f;
+            yield return null;
+        }
+
+        doFade = true;
+        yield return null;
+    }
+
+    IEnumerator DoFadeIn()
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / 1.25f;
+            yield return null;
+        }
+
+        doFade = true;
+        yield return null;
+    }
+
+}
