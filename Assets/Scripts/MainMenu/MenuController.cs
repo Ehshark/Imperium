50bda31a2701925875fc6d73fa2d4e8f338e8dfb
C067 <callumadodge@gmail.com>
2020-04-30 10:31:08 -0400

    Added MainMenu

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LeaderboardClicked() 
+    {
+        SceneManager.LoadScene("Leaderboard");
+    }
+
+    public void OptionsClicked()
+    {
+
+    }
+
+    public void CreditsClicked()
+    {
+        Application.OpenURL("https://imperium-site.herokuapp.com/Home/Credit");
+    }
+
+    public void QuitClicked()
+    {
+        Application.Quit();
+    }
+}
