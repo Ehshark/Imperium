3cd1317eca7b3a227425499f3dee15f2deba0284
Callum Dodge <callumadodge@gmail.com>
2020-05-01 12:06:58 -0400

    UpdatedLoginAndMenu

--- Assets/Scripts/Login/LoginPlayer.cs
+++ Assets/Scripts/Login/LoginPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using MySql.Data.MySqlClient;
 using System.Text;
 using System;
+using UnityEngine.SceneManagement;
 
 public class LoginPlayer : MonoBehaviour
 {
@@ -21,9 +22,6 @@ public class LoginPlayer : MonoBehaviour
     private string email;
     private string password;
 
-    //Player Object
-    protected static Player player = new Player();
-
     //Crypto
     private Crypto crypto = new Crypto();
 
@@ -34,21 +32,27 @@ public class LoginPlayer : MonoBehaviour
         password = passwordInput.text.ToString();
 
         //Using the email, get the player
-        Player tmp = GetPlayer(email);
+        Player tmp = SQLManager.GetPlayer(email);
 
         //Compare if the Player was found
         if (tmp.PLAYER_ID != 0)
         {
             if (crypto.VerifyHashedPassword(tmp.PLAYER_PASSWORD, password))
             {
-                errorText.text = "Player found: " + tmp.PLAYER_USERNAME;
-                errorPanel.SetActive(true);
+                tmp.LEADERBOARD_LEVEL = SQLManager.GetLeaderboardLevel(tmp.PLAYER_ID);
+                GameManager.player = tmp;
+                SceneManager.LoadScene("MainMenu");
             }
             else
             {
                 errorText.text = "Error: Password";
                 errorPanel.SetActive(true);
             }
+        }
+        else if (tmp.PLAYER_USERNAME == null)
+        {
+            errorText.text = "Their was a problem trying to retrieve your account. Please try again.";
+            errorPanel.SetActive(true);
         }
         else
         {
@@ -57,71 +61,6 @@ public class LoginPlayer : MonoBehaviour
                 errorText.text = "The following email doesn't seem to have an account with us. Please try again.";
                 errorPanel.SetActive(true);
             }
-        }
-    }
-
-    private Player GetPlayer(string email)
-    {
-        MySqlDataAdapter da = new MySqlDataAdapter();
-        MySqlCommand cmd;
-        MySqlDataReader rdr;
-
-        try
-        {
-            da = new MySqlDataAdapter();
-
-            //Create the SelectCommand
-            string sql = "SELECT * " +
-                         "FROM Players " +
-                         "WHERE PLAYER_EMAIL='" + email + "'";
-
-            cmd = new MySqlCommand(sql, DatabaseManager.connection);
-            rdr = cmd.ExecuteReader();
-
-            //Create a temp Player
-            Player tmpPlayer = new Player();
-
-            //Read and Store the Player if found
-            while (rdr.Read())
-            {
-                tmpPlayer.PLAYER_ID = int.Parse(rdr[0].ToString());
-                tmpPlayer.PLAYER_USERNAME = rdr[1].ToString();
-
-                if (!DBNull.Value.Equals(rdr[2]))
-                {
-                    tmpPlayer.PLAYER_AVATAR = (byte[])rdr[2];
-                }
-
-                if (!DBNull.Value.Equals(rdr[3]))
-                {
-                    tmpPlayer.PLAYER_AVATARTYPE = rdr[3].ToString();
-                }
-
-                tmpPlayer.PLAYER_PASSWORD = rdr[4].ToString();
-                tmpPlayer.PLAYER_PASSWORDCOUNT = int.Parse(rdr[5].ToString());
-                tmpPlayer.PLAYER_EMAIL = rdr[6].ToString();
-                tmpPlayer.PLAYER_LOGGEDON = (bool)rdr[7];
-                tmpPlayer.PLAYER_INGAME = (bool)rdr[8];
-                tmpPlayer.PLAYER_DATEREGISTERED = (DateTime)rdr[9];
-            }
-
-            //Close the Reader
-            rdr.Close();
-
-            //Return the Player
-            return tmpPlayer;
-        }
-        catch
-        {
-            //Create a temp Player
-            Player tmpPlayer = new Player();
-
-            //Set Error Message
-            errorText.text = "Their was a problem trying to retrieve your account. Please try again.";
-            errorPanel.SetActive(true);
-
-            //Return the Player
-            return tmpPlayer;
         }
     }
 
