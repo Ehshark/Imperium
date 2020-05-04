c68303a91f0cc1c8896e5b478923d6cf8d69ffa2
Callum Dodge <callumadodge@gmail.com>
2020-05-04 17:16:17 -0400

    Added HeroGold and Shop Scripts

---
+++ Assets/Scripts/Shop/ShopController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rightShopUI;
+    [SerializeField]
+    private TextMeshProUGUI goldCost;
+
+    public GameObject minionPrefab;
+
+    public void Start()
+    {
+        MinionData minion = Resources.Load("Minions/1") as MinionData;
+        UIManager.Instance.currentMinion = minion;
+
+        GameObject tmp = Instantiate(minionPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        tmp.transform.SetParent(GameObject.Find("DealtMinions").transform);
+        tmp.transform.localScale = new Vector3(1f, 1f, 1f);
+        tmp.transform.localPosition = new Vector3(0,0,0);
+
+        tmp.AddComponent<ShowShopCard>();
+        tmp.GetComponent<ShowShopCard>().rightShopUI = rightShopUI;
+    }
+
+    public void UpdateShop(MinionVisual selectedMinion)
+    {
+        goldCost.text = selectedMinion.cost.ToString();
+    }
+
+    public void CloseShop()
+    {
+        rightShopUI.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Shop/ShowShopCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowShopCard : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject rightShopUI;
+
+    private ShopController shop;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!rightShopUI.activeSelf)
+        {
+            rightShopUI.SetActive(true);
+            Debug.Log(eventData.selectedObject);
+            //shop.UpdateShop(eventData.selectedObject.GetComponent<MinionVisual>());
+        }
+    }
+}
