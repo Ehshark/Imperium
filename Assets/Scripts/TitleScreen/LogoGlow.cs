a7e8af9316ba467c19c405c2dc7062d0035aa92e
Callum Dodge <callumadodge@gmail.com>
2020-07-27 00:19:52 -0400

    Added animations to the Title Screen

---
+++ Assets/Scripts/TitleScreen/LogoGlow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoGlow : MonoBehaviour
+{
+    public ParticleSystem logoGlow;
+    public GameObject panel;
+
+    public void EnableGlow()
+    {
+        logoGlow.gameObject.SetActive(true);
+        panel.SetActive(true);
+    }
+}
